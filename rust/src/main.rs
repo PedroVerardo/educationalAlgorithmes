05567c1a6680a0d204e49166782d5237a9ae0cc5
Guilherme Barbosa <guibarbosa@tecgraf.puc-rio.br>
2023-10-11 20:20:52 -0300

    adding bubble sort algorithm

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,35 +1,14 @@
-use sorts::quick_sort;
-use std::fs::File;
-use std::io::Read;
-use std::mem;
-
-pub fn read_from_dat_into_vector(filename: &str) -> Vec<i32> {
-    let mut file = File::open(filename).expect("Failed to open file");
-    let file_len = file.metadata().expect("Failed to get file metadata").len() as usize;
-    let mut data: Vec<i32> = vec![0; file_len / mem::size_of::<i32>()];
-
-    let data_slice_bytes: &mut [u8] = unsafe {
-        let data_slice_i32: &mut [i32] = data.as_mut_slice();
-        let data_ptr: *mut i32 = data_slice_i32.as_mut_ptr();
-        std::slice::from_raw_parts_mut(data_ptr as *mut u8, file_len)
-    };
-
-    file.read_exact(data_slice_bytes).expect("Failed to read from file");
-
-    let data_slice_i32: &mut [i32] =
-        unsafe { mem::transmute(data_slice_bytes) };
-
-    data.truncate(data_slice_i32.len());
-
-    return data;
-}
+mod sorts;
 
 fn main(){
-    let file: &str = "/home/barbosa/Github/educationalAlgorithms/assets/";
+    let mut vec: Vec<i32> = vec![6, 5, 4, 3, 2, 1];
+    let v_len: usize = vec.len();
+    println!("Vector before quick sorting: {:?}", vec);
+    sorts::quick_sort::quick_sort(&mut vec, 0, v_len - 1);
+    println!("Vector after quick sorting: {:?}", vec);
 
-    let mut vector: Vec<i32> = read_from_dat_into_vector(file);
-    quick_sort(vector);
-
-
-    
+    let mut vec2: Vec<i32> = vec![10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+    println!("Vector before bubble sorting: {:?}", vec2);
+    sorts::bubble_sort::bubble_sort(&mut vec2);
+    println!("Vector after bubble sorting: {:?}", vec2);
 }
---
+++ rust/src/sorts/merge_sort.rs
@@ -0,0 +1,5 @@
+/* Implementation of the merge sort algorithm */
+
+pub fn merge_sort(){
+    
+}
