3f01b0035183d461a74feb1aa6ae7992491d7e39
Guilherme <guibarbosa@aluno.puc-rio.br>
2023-10-11 09:50:05 -0300

    adding a README.md file

--- python/main.py
+++ python/main.py
@@ -3,6 +3,7 @@ from sorts.quick_sort import quick_sort
 
 if __name__ == "__main__":
     path = "/home/barbosa/Github/educationalAlgorithms/assets/array_big.dat"
+    
     vec = [2,3,5,8,1,10,4,6,9]
     print(f"Printing the vector: {vec}\n")
     quick_sort(vec, 0, len(vec) - 1)
